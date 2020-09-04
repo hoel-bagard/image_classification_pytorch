be606bfb076fd5193f8f1014210d873b13fc362c
Hoel Bagard <hoel.bagard@gmail.com>
2020-09-04 14:12:34 +0900

    Tried to remove client references

--- src/dataset/dataset_utils.py
+++ src/dataset/dataset_utils.py
@@ -21,27 +21,3 @@ def dogs_vs_cats(data_path: str, label_map: Dict) -> np.ndarray:
             labels.append([image_path, key])
     labels = np.asarray(labels)
     return labels
-
-
-def chugai(data_path: str, label_map: Dict) -> np.ndarray:
-    """
-    chugai specific loading function
-    Args:
-        data_path: Path to the root folder of the dataset.
-                   This folder is expected to contain subfolders for each class, with the images inside.
-        label_map: dictionarry mapping an int to a class
-    Return:
-        numpy array containing the images' paths and the associated label
-    """
-    labels = []
-    for key in range(len(label_map)):
-        pathname = os.path.join(data_path, f"{label_map[key][0]}*", "**", "*.jpg")
-        print(pathname)
-        for image_path in glob.glob(pathname, recursive=True):
-            print(f"Loading data {image_path}   ", end="\r")
-            if "non_visible" in image_path:
-                labels.append([image_path, 0])
-            else:
-                labels.append([image_path, key])
-    labels = np.asarray(labels)
-    return labels
