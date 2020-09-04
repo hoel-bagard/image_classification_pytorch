fa12affe2c3b0421cf11fe60928736cd85dc683a
Hoel Bagard <hoel.bagard@gmail.com>
2020-09-04 14:08:39 +0900

    Added grad-cam

--- src/utils/draw.py
+++ src/utils/draw.py
@@ -1,4 +1,5 @@
 import os
+from typing import Tuple
 
 import cv2
 import numpy as np
@@ -7,13 +8,15 @@ import torch
 from config.data_config import DataConfig
 
 
-def draw_pred(imgs: torch.Tensor, predictions: torch.Tensor, labels: torch.Tensor) -> np.ndarray:
+def draw_pred(imgs: torch.Tensor, predictions: torch.Tensor, labels: torch.Tensor,
+              size: Tuple[int, int] = (480, 480), data_path: str = DataConfig.DATA_PATH) -> np.ndarray:
     """
     Draw predictions and labels on the image to help with TensorBoard visualisation.
     Args:
         imgs: Raw images.
         predictions: Predictions of the network, after softmax but before taking argmax
         labels: Labels corresponding to the images
+        size: The images will be resized to this size
     Returns: images with information written on them
     """
     imgs: np.ndarray = imgs.cpu().detach().numpy()
@@ -23,7 +26,7 @@ def draw_pred(imgs: torch.Tensor, predictions: torch.Tensor, labels: torch.Tenso
     imgs = imgs.transpose(0, 2, 3, 1)  # Conversion to H x W x C
 
     label_map = {}
-    with open(os.path.join(DataConfig.DATA_PATH, "classes.names")) as table_file:
+    with open(os.path.join(data_path, "classes.names")) as table_file:
         for key, line in enumerate(table_file):
             label = line.strip()
             label_map[key] = label
@@ -31,9 +34,11 @@ def draw_pred(imgs: torch.Tensor, predictions: torch.Tensor, labels: torch.Tenso
     new_imgs = []
     for img, preds, label in zip(imgs, predictions, labels):
         img = np.asarray(img * 255.0, dtype=np.uint8)
-        img = cv2.resize(img, (480, 480), interpolation=cv2.INTER_AREA)
+        img = cv2.resize(img, size, interpolation=cv2.INTER_AREA)
         preds = str([round(float(conf), 2) for conf in preds]) + f"  ==> {np.argmax(preds)}"
+
         img = cv2.putText(img, preds, (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 0, 0), 1, cv2.LINE_AA)
-        new_imgs.append(cv2.putText(img, f"Label: {label}  ({label_map[label]})", (20, 40),
-                                    cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 0, 0), 1, cv2.LINE_AA))
+        img = cv2.putText(img, f"Label: {label}  ({label_map[label]})", (20, 40),
+                          cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 0, 0), 1, cv2.LINE_AA)
+        new_imgs.append(img)
     return np.asarray(new_imgs)
