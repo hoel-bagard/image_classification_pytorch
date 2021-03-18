b14fd8e83d40ccd384277b6ceaaaaa1ae8ebca07
Hoel Bagard <hoel.bagard@gmail.com>
2021-03-18 20:40:01 +0900

    Redid the data augmentation part, switch train file to BatchGenerator

--- src/dataset/transforms.py
+++ src/dataset/transforms.py
@@ -1,116 +1,99 @@
 import random
+from typing import Callable
 
 import cv2
 import numpy as np
 import torch
 
 
-class Crop(object):
-    """ Crop the image """
-
-    def __init__(self, top: int = 0, bottom: int = 1, left: int = 0, right: int = 1):
-        self.top = top
-        self.bottom = bottom
-        self.left = left
-        self.right = right
-
-    def __call__(self, sample):
-        img, label = sample["img"], sample["label"]
-        cropped_img = img[self.top:-self.bottom, self.left:-self.right]
-        return {"img": cropped_img, "label": label}
+def compose_transformations(transformations: list[Callable[[np.ndarray, np.ndarray], tuple[np.ndarray, np.ndarray]]]):
+    """ Returns a function that applies all the given transformations"""
+    def compose_transformations_fn(imgs: list[np.ndarray], labels: list[np.ndarray]):
+        """ Apply transformations on a batch of data"""
+        for fn in transformations:
+            imgs, labels = fn(imgs, labels)
+        return imgs, labels
+    return compose_transformations_fn
 
 
-class RandomCrop(object):
-    """ Random crops image """
+def crop(top: int = 0, bottom: int = 1, left: int = 0, right: int = 1):
+    """ Returns a function that crops a batch of images.
 
-    def __init__(self, size_reduction_factor: int = 0.9):
-        self.size = size_reduction_factor
+    Args:
+        top (int): The number of pixels to remove from the top of the images
+        bottom (int): The number of pixels to remove from the bottom of the images
+        left (int): The number of pixels to remove from the left of the images
+        right (int): The number of pixels to remove from the right of the images
 
-    def __call__(self, sample):
-        img, label = sample["img"], sample["label"]
-        h = random.randint(0, int(img.shape[0]*(1-self.size))-1)
-        w = random.randint(0, int(img.shape[1]*(1-self.size))-1)
-        cropped_img = img[h:h+int(img.shape[0]*self.size), w:w+int(img.shape[1]*self.size)]
-
-        return {"img": cropped_img, "label": label}
-
-
-class Resize(object):
-    """ Resize the image in a sample to a given size. """
-
-    def __init__(self, width: int, height: int):
-        self.width = width
-        self.height = height
-
-    def __call__(self, sample):
-        img, label = sample['img'], sample['label']
-        img = cv2.resize(img, (self.width, self.height))
-        return {'img': img, 'label': label}
+    Returns:
+        callable: The function doing the cropping
+    """
+    def crop_fn(imgs: np.ndarray, labels: np.ndarray):
+        imgs = imgs[:, top:-bottom, left:-right]
+        return imgs, labels
+    return crop_fn
 
 
-class Normalize(object):
-    """ Normalize the image so that its values are in [0, 1] """
+def random_crop(reduction_factor: float = 0.9):
+    """ Randomly crops image """
+    def random_crop_fn(imgs: np.ndarray, labels: np.ndarray):
+        """ Randomly crops a batch of data (the "same" patch is taken across all images) """
+        h = random.randint(0, int(imgs.shape[1]*(1-reduction_factor))-1)
+        w = random.randint(0, int(imgs.shape[2]*(1-reduction_factor))-1)
+        cropped_imgs = imgs[:, h:h+int(imgs.shape[1]*reduction_factor), w:w+int(imgs.shape[2]*reduction_factor)]
+        return cropped_imgs, labels
+    return random_crop_fn
 
-    def __call__(self, sample):
-        img, label = sample['img'], sample['label']
-        return {'img': img/255.0, 'label': label}
 
-
-class VerticalFlip(object):
-    """ Randomly flips the img around the x-axis """
-
-    def __call__(self, sample):
-        img, label = sample["img"], sample["label"]
+def vertical_flip(imgs: np.ndarray, labels: np.ndarray):
+    """ Randomly flips images around the x-axis """
+    for i in range(len(imgs)):
         if random.random() > 0.5:
-            img = cv2.flip(img, 0)
-        return {"img": img, "label": label}
+            imgs[i] = cv2.flip(imgs[i], 0)
+    return imgs, labels
 
 
-class HorizontalFlip(object):
-    """ Randomly flips the img around the y-axis """
-
-    def __call__(self, sample):
-        img, label = sample["img"], sample["label"]
+def horizontal_flip(imgs: np.ndarray, labels: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    """ Randomly flips images around the y-axis """
+    for i in range(len(imgs)):
         if random.random() > 0.5:
-            img = cv2.flip(img, 1)
+            imgs[i] = cv2.flip(imgs[i], 1)
+    return imgs, labels
 
-        return {"img": img, "label": label}
 
-
-class Rotate180(object):
-    """ Randomly rotate the image by 180 degrees """
-
-    def __call__(self, sample):
-        img, label = sample["img"], sample["label"]
+def rotate180(imgs: np.ndarray, labels: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    """ Randomly rotate images by 180 degrees """
+    for i in range(len(imgs)):
         if random.random() > 0.5:
-            img = cv2.rotate(img, cv2.ROTATE_180)
-        return {"img": img, "label": label}
+            imgs[i] = cv2.rotate(imgs[i], cv2.ROTATE_180)
+    return imgs, labels
 
 
-class ToTensor(object):
-    """Convert ndarrays in sample to Tensors."""
+def to_tensor():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    def __call__(self, sample):
-        img, label = sample['img'], sample['label']
-
-        # swap color axis because
-        # numpy image: H x W x C
-        # torch image: C X H X W
-
-        img = img.transpose((2, 0, 1))
-        return {'img': torch.from_numpy(img),
-                'label': torch.from_numpy(np.asarray(label))}
+    def to_tensor_fn(imgs: np.ndarray, labels: np.ndarray) -> tuple[torch.Tensor, torch.Tensor]:
+        """Convert ndarrays in sample to Tensors."""
+        imgs = imgs.transpose((0, 3, 1, 2))
+        return torch.from_numpy(imgs).to(device), torch.from_numpy(labels).to(device)
+    return to_tensor_fn
 
 
-class Noise(object):
-    """ Add random noise to the image """
+def normalize_fn(imgs: torch.Tensor, labels: torch.Tensor):
+    """ Normalize a batch of images so that its values are in [0, 1] """
+    return imgs/255.0, labels
 
-    def __call__(self, sample):
-        img, label = sample['img'], sample['label']
-        noise_offset = (torch.rand(img.shape)-0.5)*0.05
-        noise_scale = (torch.rand(img.shape) * 0.2) + 0.9
 
-        img = img * noise_scale + noise_offset
-        img = torch.clamp(img, 0, 1)
+def noise():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-        return {'img': img, 'label': label}
+    def noise_fn(imgs: torch.Tensor, labels: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+        """ Add random noise to the images """
+        noise_offset = (torch.rand(imgs.shape, device=device)-0.5)*0.05
+        noise_scale = (torch.rand(imgs.shape, device=device) * 0.2) + 0.9
+
+        imgs = imgs * noise_scale + noise_offset
+        imgs = torch.clamp(imgs, 0, 1)
+
+        return imgs, labels
+    return noise_fn
--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 from torch.utils.tensorboard import SummaryWriter  # noqa: F401  # Needs to be there to avoid segfaults
-import argparse
+from argparse import ArgumentParser
+from pathlib import Path
 import os
-import glob
 import shutil
 import time
 
@@ -11,99 +11,104 @@ from torchsummary import summary
 
 from config.data_config import DataConfig
 from config.model_config import ModelConfig
-from src.dataset.dataset import Dataset
-import src.dataset.transforms as transforms
+from src.torch_utils.utils.batch_generator import BatchGenerator
+from src.dataset.defeault_loader import (
+    default_loader,
+    default_load_data,
+    default_load_labels
+)
+import src.dataset.data_transformations as transforms
+from src.torch_utils.utils.misc import get_config_as_dict
 from src.networks.build_network import build_model
+from src.torch_utils.utils.misc import clean_print
 from src.train import train
 
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = ArgumentParser()
     parser.add_argument("--limit", default=None, type=int, help="Limits the number of apparition of each class")
     parser.add_argument("--load_data", action="store_true", help="Loads all the videos into RAM")
     args = parser.parse_args()
 
     if not DataConfig.KEEP_TB:
-        while os.path.exists(DataConfig.TB_DIR):
+        while DataConfig.TB_DIR.exists():
             shutil.rmtree(DataConfig.TB_DIR, ignore_errors=True)
             time.sleep(0.5)
-    os.makedirs(DataConfig.TB_DIR, exist_ok=True)
+    DataConfig.TB_DIR.mkdir(parents=True, exist_ok=True)
 
     if DataConfig.USE_CHECKPOINT:
         if not DataConfig.KEEP_CHECKPOINTS:
-            while os.path.exists(DataConfig.CHECKPOINT_DIR):
+            while DataConfig.CHECKPOINT_DIR.exists():
                 shutil.rmtree(DataConfig.CHECKPOINT_DIR, ignore_errors=True)
                 time.sleep(0.5)
         try:
-            os.makedirs(DataConfig.CHECKPOINT_DIR, exist_ok=False)
+            DataConfig.CHECKPOINT_DIR.mkdir(parents=True, exist_ok=True)
         except FileExistsError:
             print(f"The checkpoint dir {DataConfig.CHECKPOINT_DIR} already exists")
             return -1
 
         # Makes a copy of all the code (and config) so that the checkpoints are easy to load and use
-        output_folder = os.path.join(DataConfig.CHECKPOINT_DIR, "Classification-PyTorch")
-        for filepath in glob.glob(os.path.join("**", "*.py"), recursive=True):
-            destination_path = os.path.join(output_folder, filepath)
-            os.makedirs(os.path.dirname(destination_path), exist_ok=True)
+        output_folder = DataConfig.CHECKPOINT_DIR / "Segmentation-PyTorch"
+        for filepath in list(Path(".").glob("**/*.py")):
+            destination_path = output_folder / filepath
+            destination_path.parent.mkdir(parents=True, exist_ok=True)
             shutil.copy(filepath, destination_path)
-        # shutil.copytree(".git", os.path.join(output_folder, ".git"))
         misc_files = ["README.md", "requirements.txt", "setup.cfg", ".gitignore"]
         for misc_file in misc_files:
-            shutil.copy(misc_file, os.path.join(output_folder, misc_file))
+            shutil.copy(misc_file, output_folder / misc_file)
         print("Finished copying files")
 
+    torch.backends.cudnn.benchmark = True   # Makes training quite a bit faster
 
+    # Data augmentation done on cpu.
+    augmentation_pipeline = transforms.compose_transformations((
+        transforms.vertical_flip,
+        transforms.horizontal_flip,
+        transforms.rotate180,
+    ))
+    # GPU pipeline used by both validation and train
+    base_gpu_pipeline = (
+        transforms.to_tensor(),
+        transforms.normalize(labels_too=True),
+    )
+    train_gpu_augmentation_pipeline = transforms.compose_transformations((
+        *base_gpu_pipeline,
+        transforms.noise()
+    ))
 
+    train_data, train_labels = default_loader(DataConfig.DATA_PATH / "Train",
+                                              limit=args.limit, load_data=args.load_data,
+                                              data_preprocessing_fn=default_load_data if args.load_data else None,
+                                              labels_preprocessing_fn=default_load_labels if args.load_data else None)
+    clean_print("Train data loaded")
 
-    # TODO: Have nb_workers in the config
-    # TODO: Have a data  loading helper in the condig ?
-    from src.dataset.dice_loader import load_dice
-    from src.torch_utils.utils.batch_generator import BatchGenerator
-    data, labels = load_dice(DataConfig.DATA_PATH / "Train", limit=args.limit, load_data=args.load_data)
+    val_data, val_labels = default_loader(DataConfig.DATA_PATH / "Validation",
+                                          limit=args.limit, load_data=args.load_data,
+                                          data_preprocessing_fn=default_load_data if args.load_data else None,
+                                          labels_preprocessing_fn=default_load_labels if args.load_data else None)
+    clean_print("Validation data loaded")
 
-    data_preprocessing_fn = 1 if args.load_data else 2
-    train_dataloader = BatchGenerator(data, labels, ModelConfig.BATCH_SIZE, nb_workers=4, shuffle=True)
+    with BatchGenerator(train_data, train_labels,
+                        ModelConfig.BATCH_SIZE, nb_workers=DataConfig.NB_WORKERS,
+                        data_preprocessing_fn=default_load_data if not args.load_data else None,
+                        labels_preprocessing_fn=default_load_labels if not args.load_data else None,
+                        aug_pipeline=augmentation_pipeline,
+                        gpu_augmentation_pipeline=train_gpu_augmentation_pipeline,
+                        shuffle=True) as train_dataloader, \
+        BatchGenerator(val_data, val_labels, ModelConfig.BATCH_SIZE, nb_workers=DataConfig.NB_WORKERS,
+                       data_preprocessing_fn=default_load_data if not args.load_data else None,
+                       labels_preprocessing_fn=default_load_labels if not args.load_data else None,
+                       gpu_augmentation_pipeline=transforms.compose_transformations(base_gpu_pipeline),
+                       shuffle=False) as val_dataloader:
 
+        print(f"\nLoaded {len(train_dataloader)} train data and",
+              f"{len(val_dataloader)} validation data", flush=True)
 
-    train_dataset = Dataset(os.path.join(DataConfig.DATA_PATH, "Train"),
-                            limit=args.limit,
-                            load_images=args.load_data,
-                            transform=Compose([
-                                transforms.Crop(top=600, bottom=500, left=800, right=200),
-                                transforms.RandomCrop(0.98),
-                                transforms.Resize(*ModelConfig.IMAGE_SIZES),
-                                transforms.Normalize(),
-                                transforms.VerticalFlip(),
-                                transforms.HorizontalFlip(),
-                                transforms.Rotate180(),
-                                transforms.ToTensor(),
-                                transforms.Noise()
-                            ]))
-    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=ModelConfig.BATCH_SIZE,
-                                                   shuffle=True, num_workers=8)
+        print("Building model. . .", end="\r")
+        model = build_model(ModelConfig.MODEL, DataConfig.OUTPUT_CLASSES, **get_config_as_dict(ModelConfig))
+        summary(model, train_dataloader.data_shape)
 
-    print("Train data loaded" + ' ' * (os.get_terminal_size()[0] - 17))
-
-    val_dataset = Dataset(os.path.join(DataConfig.DATA_PATH, "Validation"),
-                          limit=args.limit,
-                          load_images=args.load_data,
-                          transform=Compose([
-                              transforms.Crop(top=600, bottom=500, left=800, right=200),
-                              transforms.Resize(*ModelConfig.IMAGE_SIZES),
-                              transforms.Normalize(),
-                              transforms.ToTensor()
-                          ]))
-    val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=ModelConfig.BATCH_SIZE,
-                                                 shuffle=True, num_workers=8)
-    print("Validation data loaded" + ' ' * (os.get_terminal_size()[0] - 22))
-
-    print(f"\nLoaded {len(train_dataloader.dataset)} train data and",
-          f"{len(val_dataloader.dataset)} validation data", flush=True)
-
-    model = build_model(ModelConfig.NETWORK)
-    summary(model, (3, ModelConfig.IMAGE_SIZES[0], ModelConfig.IMAGE_SIZES[1]))
-
-    train(model, train_dataloader, val_dataloader)
+        train(model, train_dataloader, val_dataloader)
 
 
 if __name__ == '__main__':
