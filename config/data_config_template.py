febb2d3e5c9825ec299d386e48fdd83f1d401f54
Hoel Bagard <hoel.bagard@gmail.com>
2021-03-18 20:58:08 +0900

    Updated config files

--- config/data_config_template.py
+++ config/data_config_template.py
@@ -1,22 +1,26 @@
+from pathlib import Path
 import os
 
 
 class DataConfig:
     # Recording part
-    DATA_PATH          = 'path/to/dataset'  # Path to the dataset folder
+    DATA_PATH          = Path("path", "to", "dataset")  # Path to the dataset folder
     USE_CHECKPOINT     = True               # Whether to save checkpoints or not
-    CHECKPOINT_DIR     = 'path/to/checkpoint_dir/AI_Name'  # Path to checkpoint dir
+    CHECKPOINT_DIR     = Path("path", "to", "checkpoint_dir", "AI_Name")  # Path to checkpoint dir
     CHECKPT_SAVE_FREQ  = 10                  # How often to save checkpoints (if they are better than the previous one)
     KEEP_CHECKPOINTS   = True                # Whether to remove the checkpoint dir
     USE_TB             = True                # Whether generate a TensorBoard or not
-    TB_DIR             = 'path/to/log_dir/AI_Name'  # TensorBoard dir
+    TB_DIR             = Path("path", "to", "log_dir", "AI_Name")  # TensorBoard dir
     KEEP_TB            = True                # Whether to remove the TensorBoard dir
     VAL_FREQ           = 20                  # How often to compute accuracy and images (also used for validation freq)
     RECORD_START       = 10                  # Checkpoints and TensorBoard are not recorded before this epoch
 
+    # Dataloading
+    NB_WORKERS = int(os.cpu_count() * 0.8)  # Number of workers to use for dataloading
+
     # Build a map between id and names
-    LABEL_MAP = {}
-    with open(os.path.join(DATA_PATH, "classes.names")) as table_file:
-        for key, line in enumerate(table_file):
-            label = line.strip()
-            LABEL_MAP[key] = label
+    LABEL_MAP = {}   # Maps an int to a class name
+    with open(DATA_PATH / "classes.names") as text_file:
+        for key, line in enumerate(text_file):
+            LABEL_MAP[key] = line.strip()
+    NB_CLASSES = len(LABEL_MAP)
--- config/model_config_template.py
+++ config/model_config_template.py
@@ -1,3 +1,10 @@
+from typing import Union
+
+from src.networks.build_network import (
+    ModelHelper
+)
+
+
 class ModelConfig:
     # Training parameters
     BATCH_SIZE         = 16            # Batch size
@@ -7,12 +14,14 @@ class ModelConfig:
     DECAY_START        = 20
     REG_FACTOR         = 0.005       # Regularization factor (Used to be 0.005 for the fit mode)
 
+    # Data processing
+    IMAGE_SIZES: tuple[int, int] = (256, 256)
+
     # Network part
-    NETWORK = "WideNet"  # Name of the network to use
-    CHANNELS = [3, 8, 16, 32, 32, 16]
-    SIZES = [5, 3, 3, 3, 3, 3]   # Kernel sizes
-    STRIDES = [5, 3, 3, 2, 2, 2]
-    PADDINGS = [2, 1, 1, 1, 1, 1]
-    NB_BLOCKS = [1, 2, 2, 1, 1, 1]
-    IMAGE_SIZES = (2448, 2048)  # All images will be resized to this size
-    OUTPUT_CLASSES = 5
+    MODEL = ModelHelper.WideNet
+
+    CHANNELS: list[int] = [3, 8, 16, 32, 32, 16]
+    SIZES: list[Union[int, tuple[int, int]]]  = [5, 3, 3, 3, 3, 3]   # Kernel sizes
+    STRIDES: list[Union[int, tuple[int, int]]]  = [5, 3, 3, 2, 2, 2]
+    PADDINGS: list[Union[int, tuple[int, int]]]  = [2, 1, 1, 1, 1, 1]
+    BLOCKS: list[int] = [1, 2, 2, 1, 1, 1]
