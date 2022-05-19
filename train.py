b94c15a10a7cdfe39b07bb25e0080432035799c3
Hoel Bagard <hoel.bagard@gmail.com>
2022-05-19 21:02:17 +0900

    Renamed configs in the train file

--- train.py
+++ train.py
@@ -6,19 +6,20 @@ from functools import partial
 from pathlib import Path
 
 import torch
-from torchsummary import summary
 
 import src.dataset.data_transformations as transforms
-from config.data_config import DataConfig
-from config.model_config import ModelConfig
+from config.data_config import get_data_config
+from config.model_config import get_model_config
 from src.dataset.default_loader import default_load_data
 from src.dataset.default_loader import default_loader as data_loader
 # from src.dataset.dataset_loaders import name_loader as data_loader
 from src.networks.build_network import build_model
 from src.torch_utils.utils.batch_generator import BatchGenerator
-from src.torch_utils.utils.logger import create_logger, DummyLogger
+from src.torch_utils.utils.logger import create_logger
 from src.torch_utils.utils.misc import clean_print
 from src.torch_utils.utils.misc import get_config_as_dict
+from src.torch_utils.utils.prepare_folders import prepare_folders
+from src.torch_utils.utils.torch_summary import summary
 from src.train import train
 
 
@@ -29,27 +30,39 @@ def main():
     parser.add_argument("--load_data", "--ld", action="store_true", help="Loads all the images into RAM")
     parser.add_argument("--name", type=str, default="Train",
                         help="Used to know what a train is when using ps. Also name of the logger.")
+    parser.add_argument("--verbose_level", "-v", choices=["debug", "info", "error"], default="info", type=str,
+                        help="Logger level.")
     args = parser.parse_args()
 
-    if not DataConfig.KEEP_TB:
-        while DataConfig.TB_DIR.exists():
-            shutil.rmtree(DataConfig.TB_DIR, ignore_errors=True)
-            time.sleep(0.5)
-    DataConfig.TB_DIR.mkdir(parents=True, exist_ok=True)
+    limit: int = args.limit
+    name: str = args.name
+    verbose_level: str = args.verbose_level
 
-    if DataConfig.USE_CHECKPOINT:
-        if not DataConfig.KEEP_CHECKPOINTS:
-            while DataConfig.CHECKPOINT_DIR.exists():
-                shutil.rmtree(DataConfig.CHECKPOINT_DIR, ignore_errors=True)
+    data_config = get_data_config()
+    model_config = get_model_config()
+
+    log_dir = data_config.CHECKPOINTS_DIR / "print_logs" if data_config.USE_CHECKPOINTS else None
+    logger = create_logger(name, log_dir=log_dir, verbose_level=verbose_level)
+
+    if not data_config.KEEP_TB:
+        while data_config.TB_DIR.exists():
+            shutil.rmtree(data_config.TB_DIR, ignore_errors=True)
+            time.sleep(0.5)
+    data_config.TB_DIR.mkdir(parents=True, exist_ok=True)
+
+    if data_config.USE_CHECKPOINT:
+        if not data_config.KEEP_CHECKPOINTS:
+            while data_config.CHECKPOINT_DIR.exists():
+                shutil.rmtree(data_config.CHECKPOINT_DIR, ignore_errors=True)
                 time.sleep(0.5)
         try:
-            DataConfig.CHECKPOINT_DIR.mkdir(parents=True, exist_ok=True)
+            data_config.CHECKPOINT_DIR.mkdir(parents=True, exist_ok=True)
         except FileExistsError:
-            print(f"The checkpoint dir {DataConfig.CHECKPOINT_DIR} already exists")
+            print(f"The checkpoint dir {data_config.CHECKPOINT_DIR} already exists")
             return -1
 
         # Makes a copy of all the code (and config) so that the checkpoints are easy to load and use
-        output_folder = DataConfig.CHECKPOINT_DIR / "Classification-PyTorch"
+        output_folder = data_config.CHECKPOINT_DIR / "Classification-PyTorch"
         for filepath in list(Path(".").glob("**/*.py")):
             destination_path = output_folder / filepath
             destination_path.parent.mkdir(parents=True, exist_ok=True)
@@ -57,16 +70,14 @@ def main():
         misc_files = ["README.md", "requirements.txt", "setup.cfg", ".gitignore"]
         for misc_file in misc_files:
             shutil.copy(misc_file, output_folder / misc_file)
-        logger = create_logger(args.name, DataConfig.CHECKPOINT_DIR / "logs")
         print("Finished copying files")
-    else:
-        logger = DummyLogger()
+    logger.info("Finished preparing tensorboard and checkpoints folders.")
 
     torch.backends.cudnn.benchmark = True   # Makes training quite a bit faster
 
     # Data augmentation done on cpu.
     base_cpu_pipeline = (
-        # transforms.resize(ModelConfig.IMAGE_SIZES),
+        # transforms.resize(model_config.IMAGE_SIZES),
     )
     cpu_augmentation_pipeline = transforms.compose_transformations((
         *base_cpu_pipeline,
@@ -83,19 +94,19 @@ def main():
     base_gpu_pipeline = (
         transforms.to_tensor(),
         transforms.normalize_fn,
-        transforms.padding(ModelConfig.IMAGE_SIZES),
+        transforms.padding(model_config.IMAGE_SIZES),
     )
     gpu_augmentation_pipeline = transforms.compose_transformations((
         *base_gpu_pipeline,
         transforms.noise()
     ))
 
-    train_data, train_labels = data_loader(DataConfig.DATA_PATH / "Train", DataConfig.LABEL_MAP,
+    train_data, train_labels = data_loader(data_config.DATA_PATH / "Train", data_config.LABEL_MAP,
                                            limit=args.limit, load_data=args.load_data,
                                            data_preprocessing_fn=default_load_data if args.load_data else None)
     clean_print("Train data loaded")
 
-    val_data, val_labels = data_loader(DataConfig.DATA_PATH / "Validation", DataConfig.LABEL_MAP,
+    val_data, val_labels = data_loader(data_config.DATA_PATH / "Validation", data_config.LABEL_MAP,
                                        limit=args.limit, load_data=args.load_data,
                                        data_preprocessing_fn=default_load_data if args.load_data else None,
                                        shuffle=True)
@@ -103,12 +114,12 @@ def main():
     print("Constructing dataloaders. . .", end="\r")
 
     with BatchGenerator(train_data, train_labels,
-                        ModelConfig.BATCH_SIZE, nb_workers=DataConfig.NB_WORKERS,
+                        model_config.BATCH_SIZE, nb_workers=data_config.NB_WORKERS,
                         data_preprocessing_fn=default_load_data if not args.load_data else None,
                         cpu_pipeline=cpu_augmentation_pipeline,
                         gpu_pipeline=gpu_augmentation_pipeline,
                         shuffle=True) as train_dataloader, \
-        BatchGenerator(val_data, val_labels, ModelConfig.BATCH_SIZE, nb_workers=DataConfig.NB_WORKERS,
+        BatchGenerator(val_data, val_labels, model_config.BATCH_SIZE, nb_workers=data_config.NB_WORKERS,
                        data_preprocessing_fn=default_load_data if not args.load_data else None,
                        cpu_pipeline=transforms.compose_transformations(base_cpu_pipeline),
                        gpu_pipeline=transforms.compose_transformations(base_gpu_pipeline),
@@ -121,7 +132,7 @@ def main():
                     f"{len(val_dataloader)} validation data\n")
 
         print("Building model. . .", end="\r")
-        model = build_model(ModelConfig.MODEL, DataConfig.NB_CLASSES, **get_config_as_dict(ModelConfig))
+        model = build_model(model_config.MODEL, data_config.NB_CLASSES, **get_config_as_dict(model_config))
         summary(model, train_dataloader.data_shape)
 
         train(model, train_dataloader, val_dataloader, logger)
