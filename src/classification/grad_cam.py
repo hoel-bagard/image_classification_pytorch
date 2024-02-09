0b06946267c6d6ebe939e83f0d7d5b1ea3747a47
Hoel Bagard <hoel.bagard@gmail.com>
2024-02-09 23:00:47 +0900

    some gradcam fixes

--- src/classification/grad_cam.py
+++ src/classification/grad_cam.py
@@ -4,8 +4,6 @@ from pathlib import Path
 import cv2
 import numpy as np
 import torch
-from config.record_config import DataConfig
-from config.train_config import ModelConfig
 
 import classification.data.data_transformations as transforms
 from classification.data.dataset_loaders import dog_vs_cat_loader as data_loader
@@ -13,7 +11,7 @@ from classification.data.defeault_loader import default_load_data
 from classification.networks.build_network import build_model
 from classification.torch_utils.utils.draw import draw_pred_img
 from classification.torch_utils.utils.misc import clean_print, get_config_as_dict
-
+from classification.configs import RecordConfig, TrainConfig
 
 def main() -> None:
     parser = ArgumentParser()
@@ -21,10 +19,23 @@ def main() -> None:
     parser.add_argument("data_path", type=Path, help="Path to the test dataset")
     args = parser.parse_args()
 
+    classes_names_path: Path | None = args.classes_names_path
+    classes_names: list[str] | None = args.classes_names
+
+    record_config = RecordConfig()
+    if classes_names_path is not None:
+        train_config = TrainConfig.from_classes_path(classes_names_path)
+    elif classes_names is not None:
+        train_config = TrainConfig.from_classes_names(classes_names)
+    else:
+        msg = "Either --classes_names_path or --classes_names must be provided"
+        raise ValueError(msg)
+
+
     # Creates and load the model
     model = build_model(
-        ModelConfig.MODEL,
-        DataConfig.NB_CLASSES,
+        train_config.MODEL,
+        train_config.NB_CLASSES,
         model_path=args.model_path,
         eval=True,
         **get_config_as_dict(ModelConfig),
@@ -32,9 +43,9 @@ def main() -> None:
     print("Weights loaded", flush=True)
 
     data, labels, paths = data_loader(
-        args.data_path, DataConfig.LABEL_MAP, data_preprocessing_fn=default_load_data, return_img_paths=True
+        args.data_path, train_config.LABEL_MAP, data_preprocessing_fn=default_load_data, return_img_paths=True
     )
-    base_cpu_pipeline = (transforms.resize(ModelConfig.IMAGE_SIZES),)
+    base_cpu_pipeline = (transforms.resize(train_config.IMAGE_SIZES),)
     base_gpu_pipeline = (transforms.to_tensor(), transforms.normalize(labels_too=True))
     data_transformations = transforms.compose_transformations((*base_cpu_pipeline, *base_gpu_pipeline))
     print("\nData loaded", flush=True)
@@ -72,12 +83,12 @@ def main() -> None:
             cam += w * activations[i, :, :]
 
         cam = np.maximum(cam, 0)
-        cam = cv2.resize(cam, ModelConfig.IMAGE_SIZES)
+        cam = cv2.resize(cam, train_config.IMAGE_SIZES)
         cam = cam - np.min(cam)
         cam = cam / np.max(cam)
 
         # Draw prediction (logits) on the image
-        img = draw_pred_img(img, output, label, DataConfig.LABEL_MAP, size=ModelConfig.IMAGE_SIZES)
+        img = draw_pred_img(img, output, label, train_config.LABEL_MAP, size=train_config.IMAGE_SIZES)
 
         # Fuse input image and gradcam mask
         heatmap = cv2.applyColorMap(np.uint8(255 * cam), cv2.COLORMAP_JET)
