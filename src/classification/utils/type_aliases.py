4ccee6f2cb95314c4d34a54b3479af1e2b161a5b
Hoel Bagard <hoel.bagard@gmail.com>
2024-02-12 21:28:35 +0900

    feat: add LabelArrayOrTensor and ImgArrayOrTensor type aliases.

--- src/classification/utils/type_aliases.py
+++ src/classification/utils/type_aliases.py
@@ -1,14 +1,16 @@
 from typing import TypeAlias, TypeVar
+
 import numpy as np
 import numpy.typing as npt
 import torch
 
 LabelDtype: TypeAlias = np.uint32
 LabelArray: TypeAlias =  npt.NDArray[LabelDtype]
+LabelArrayOrTensor: TypeAlias = LabelArray | torch.Tensor
 StandardizedImgDType: TypeAlias = np.float64
 ImgDType: TypeAlias = StandardizedImgDType | np.uint8
 ImgRaw: TypeAlias = npt.NDArray[np.uint8]
 ImgStandardized: TypeAlias = npt.NDArray[np.float64]
 ImgArray: TypeAlias = ImgRaw | ImgStandardized
 ImgArrayT = TypeVar("ImgArrayT", ImgRaw, ImgStandardized)
-ArrayOrTensor: TypeAlias = npt.NDArray[np.float64] | torch.Tensor
+ImgArrayOrTensor: TypeAlias = ImgArray | torch.Tensor
