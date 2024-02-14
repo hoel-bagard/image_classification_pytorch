64802b43e738f1dc84c10d8770578835c06078b7
Hoel Bagard <hoel.bagard@gmail.com>
2024-02-14 18:06:15 +0900

    refactor: re-wrote gradcam script using the grad-cam package

--- src/classification/grad_cam.py
+++ src/classification/grad_cam.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import os
 import random
 import typing
 from argparse import ArgumentParser
@@ -12,16 +13,17 @@ import albumentations
 import cv2
 import numpy as np
 import torch
-from hbtools import create_logger
+from hbtools import create_logger, show_img, yes_no_prompt
+from pytorch_grad_cam import GradCAM
+from pytorch_grad_cam.utils.image import show_cam_on_image
 
 import classification.data.data_transformations as transforms
 from classification.configs import LOGGER_NAME, TrainConfig
 from classification.data.default_loader import default_load_data
 from classification.data.default_loader import default_loader as data_loader
-from classification.networks.build_network import build_model
-from classification.torch_utils.utils.draw import draw_pred_img
+from classification.networks.build_network import build_model, ModelHelper
 from classification.torch_utils.utils.misc import clean_print, get_dataclass_as_dict
-from classification.utils.type_aliases import ImgRaw, ImgStandardized
+from classification.utils.type_aliases import ImgRaw
 
 
 def main() -> None:
@@ -35,6 +37,7 @@ def main() -> None:
     parser.add_argument(
         "--classes_names", type=str, default=None, nargs="*", help="Path to a file containing the classes names."
     )
+    parser.add_argument("--show", "--s", action="store_true", help="Show the gradcam images.")
     parser.add_argument(
         "--verbose_level", "-v", choices=["debug", "info", "error"], default="info", type=str, help="Logger level."
     )
@@ -44,6 +47,7 @@ def main() -> None:
     limit: int | None = args.limit
     classes_names_path: Path | None = args.classes_names_path
     classes_names: list[str] | None = args.classes_names
+    show: bool = args.show
     verbose_level: Literal["debug", "info", "error"] = args.verbose_level
 
     if classes_names_path is not None:
@@ -57,9 +61,8 @@ def main() -> None:
     # Set random
     torch.manual_seed(42)
     random.seed(0)
-    np.random.seed(0)
+    np.random.seed(0)  # noqa: NPY002
 
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     logger = create_logger(LOGGER_NAME, verbose_level=verbose_level)
 
     # Creates and load the model
@@ -71,18 +74,25 @@ def main() -> None:
     )
     logger.info("Weights loaded")
 
+    match train_config.MODEL:
+        case ModelHelper.ResNet34 | ModelHelper.ResNet50:
+            target_layers = [model.layer4[-1]]
+        case _:
+            msg = "GradCAM is not implemented for this model"
+            raise NotImplementedError(msg)
+    cam = GradCAM(model=model, target_layers=target_layers)
+
     imgs_paths, labels = data_loader(data_path, train_config.LABEL_MAP, limit=limit)
     nb_imgs = len(imgs_paths)
     logger.info("Data loaded")
 
-
     resize_fn = typing.cast(
-        Callable[[ImgRaw], ImgStandardized],
-        partial(cv2.resize, dsize=train_config.IMAGE_SIZES, interpolation=cv2.INTER_LINEAR)
+        Callable[[ImgRaw], ImgRaw],
+        partial(cv2.resize, dsize=train_config.IMAGE_SIZES, interpolation=cv2.INTER_LINEAR),
     )
     standardize_fn = transforms.albumentation_batch_wrapper(
         albumentations.Normalize(mean=train_config.IMG_MEAN, std=train_config.IMG_STD, p=1.0),
-    )  # TODO: Do the normalization on GPU.
+    )
     to_tensor_fn = transforms.to_tensor()
 
     for i, (img_path, label) in enumerate(zip(imgs_paths, labels, strict=True)):
@@ -91,52 +101,19 @@ def main() -> None:
         standardized_img, label_batched = standardize_fn(np.expand_dims(img, 0), np.asarray([label]))
         img_tensor, _label_tensor = to_tensor_fn(standardized_img, label_batched)
 
-        # Feed the image to the model
-        output = model(img_tensor)
-        output = torch.nn.functional.softmax(output, dim=-1)
+        # If targets is None, the highest scoring category will be used for every image in the batch.
+        grayscale_cam = cam(input_tensor=img_tensor, targets=None, eigen_smooth=True)
 
-        # Get top prediction and turn it into a one hot
-        prediction = output.argmax(dim=1)
-        one_hot = np.zeros((1, output.size()[-1]), dtype=np.float32)
-        one_hot[0][prediction] = 1
-        one_hot = torch.from_numpy(one_hot).requires_grad_(True)
-        one_hot = torch.sum(one_hot.to(device) * output)
+        # In this example grayscale_cam has only one image in the batch:
+        grayscale_cam = grayscale_cam[0, :]
+        visualization = show_cam_on_image(img / 255, grayscale_cam, use_rgb=True)
 
-        # Get gradients and activations
-        model.zero_grad()
-        one_hot.backward(retain_graph=True)
-        grads_val = model.get_gradients()[-1].cpu().data.numpy()
+        prediction = int(cam.outputs.argmax(dim=1).cpu().detach().numpy())
 
-        activations = model.get_activations()
-        activations = activations.cpu().data.numpy()[0, :]
-
-        # Make gradcam mask
-        weights = np.mean(grads_val, axis=(1, 2))
-        cam = np.zeros(activations.shape[1:], dtype=np.float32)
-
-        for i, w in enumerate(weights):
-            cam += w * activations[i, :, :]
-
-        cam = np.maximum(cam, 0)
-        cam = cv2.resize(cam, train_config.IMAGE_SIZES)
-        cam = cam - np.min(cam)
-        cam = cam / np.max(cam)
-
-        # Draw prediction (logits) on the image
-        img = draw_pred_img(img, output, label, train_config.LABEL_MAP, size=train_config.IMAGE_SIZES)
-
-        # Fuse input image and gradcam mask
-        heatmap = cv2.applyColorMap(np.uint8(255 * cam), cv2.COLORMAP_JET)
-        heatmap = np.float32(heatmap)
-        cam = heatmap + np.float32(img)
-        cam = cam / np.max(cam)
-
-        while True:
-            cv2.imshow("Image", cam)
-            key = cv2.waitKey(10)
-            if key == ord("q"):
-                cv2.destroyAllWindows()
-                break
+        if show:
+            show_img(visualization, f"{img_path=}, {prediction=}")
+            if "DISPLAY" not in os.environ:
+                yes_no_prompt("Show next image ?")
 
 
 if __name__ == "__main__":
